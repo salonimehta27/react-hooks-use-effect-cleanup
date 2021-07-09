0d2012069d65bd6d052a2a78543adbe1ac190640
Saloni <salonimehta2127@gmail.com>
2021-07-09 01:49:07 -0400

    Completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -4,10 +4,15 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerId = setInterval(() => {
       setTime(new Date());
     }, 1000);
+    //clean up function
+    return function cleanup() {
+      clearInterval(timerId)
+    }
   }, []);
+
 
   return <div>{time.toString()}</div>;
 }
